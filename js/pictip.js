e984db1764f9538abfec0eff2fc124916130feea
winjeysong <winjey-song@163.com>
2017-06-20 23:57:37 +0800

    Finish the component
    1. $(window).width —> $(window).width()
    2. Other changes

--- js/pictip.js
+++ js/pictip.js
@@ -8,7 +8,7 @@ function detailedPicTip(ele){
     let x = 10;
     let y = 20;
     //获取窗口的宽度
-    let winWidth = $(window).width;
+    let winWidth = $(window).width();
     //获取图片水平方向位置
     //let picLeft = $(this).offset().left;
 
@@ -19,16 +19,16 @@ function detailedPicTip(ele){
         var imgName = this.curTitle? this.curTitle:"";
 
         //当鼠标移入时，创建一个div提示块
-        var divTip = "<div id='divTip'><img src='" + this.href + "' alt='大图显示' width='900' height='600' />" + "<br/>" + imgName + e.pageX + "</div>";
+        var divTip = "<div id='divTip'><img src='" + this.href + "' alt='大图显示' width='900' height='600' />" + "<br/>" + imgName + "</div>";
         $("body").append(divTip);
-        //设置div块的位置
+        //设置div块的位置，并判断浮出层在鼠标移动时是否会超出屏幕
         if(e.pageX + x + 940 <= winWidth){
             $("#divTip").css({
             "top": e.pageY + y + "px",
             "left": e.pageX + x + "px"
             }).show();
         }
-        else{
+        else{  //如果超出屏幕，则固定其水平方向显示位置
             $("#divTip").css({
                 "top": e.pageY + y + "px",
                 "left": winWidth - 940 + "px"
@@ -41,11 +41,7 @@ function detailedPicTip(ele){
         this.title = this.curTitle;
         $("#divTip").remove();
     }).mousemove(function(e){
-        //鼠标移动时，提示块也会跟随移动
-        /*$("#divTip").css({
-            "top": e.pageY + x + "px",
-            "left": e.pageX + y + "px"
-        });*/
+        //鼠标移动时，提示块也会跟随移动，判断方式同前
         if(e.pageX + x + 940 <= winWidth){
             $("#divTip").css({
             "top": e.pageY + y + "px",
