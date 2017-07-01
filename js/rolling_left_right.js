5ca339a9bbbc0f5fdf17253e5cf9add4df1542d3
winjeysong <winjey-song@163.com>
2017-07-01 23:50:56 +0800

    Finish the rolling display demo

--- js/rolling_left_right.js
+++ js/rolling_left_right.js
@@ -14,7 +14,7 @@ var totalList = $innerContent.find("li").length;  //所有的图片数
 var totalPage = Math.ceil(totalList / num);  //计算有多少版面，即有多少圆点，向上取整
 
 var Rolling = {
-    //计算提示圆点的个数，并生成对应个数的圆点
+    //计算圆点的个数，并生成对应个数的圆点
     dotTip: function(appendEle){
         let totalDot = totalPage;
         let i = 0;
@@ -27,28 +27,55 @@ var Rolling = {
         }
     },
 
-    //向右箭头功能:点击后展示下一版面
-    rollingRight: function(){
-        let cur = 1;  //初始化当前页数           
-        $("span.next").click(function(){
-            if(cur == totalPage){  //如果当前是最后一个版面，则点击后重新跳转到第一个版面
-                $innerContent.animate({
-                    opacity: 0,
-                    left: 0}, 0)
-                    .animate({opacity:1},"normal");
-                cur = 1;
-            }
-            else{
-                $innerContent.animate({
-                    left: "-=" + rollingWidth  //切换版面
-                }, "normal");
-                cur += 1;
-            }
+    //当前圆点
+    curDot: function(curDot){
+        $wrap.find("span").eq(curDot - 1)  //选取当前页的圆点
+        .addClass("cur_page")  //添加当前页样式
+        .siblings().removeClass("cur_page")  //移除同辈样式
+    },
 
-            //圆点提示
-            $wrap.find("span").eq(cur - 1)  //选取当前页的圆点
-            .addClass("cur_page")  //添加当前页样式
-            .siblings().removeClass("cur_page")  //移除同辈样式
-        });            
-    },        
+    //左右箭头功能：点击后展示下一版面或上一版面
+    rollingRightLeft: function(){
+        let cur = 1;  //初始化当前页数     
+
+        //右箭头      
+        $("span.next").click(function(){
+            if(! $innerContent.is(":animated")){  //如果元素当前不处于动画中，才为它添加动画。防止快速点击鼠标后，即使放开鼠标后，还是继续产生滚动动画的情况发生
+                if(cur == totalPage){  //如果当前是最后一个版面，则点击后重新跳转到第一个版面
+                    $innerContent.animate({
+                        opacity: 0,
+                        left: 0}, 0)
+                        .animate({opacity:1},"normal");
+                    cur = 1;
+                }
+                else{
+                    $innerContent.animate({
+                        left: "-=" + rollingWidth  //切换版面
+                    }, "normal");
+                    cur += 1;
+                }
+                Rolling.curDot(cur);  //当前版面的圆点提示
+            }
+        });  
+
+        //左箭头
+        $("span.prev").click(function(){
+            if(! $innerContent.is(":animated")){  //如果元素当前不处于动画中，才为它添加动画。防止快速点击鼠标后，即使放开鼠标后，还是继续产生滚动动画的情况发生
+                if(cur == 1){  //如果当前是第一个版面，则点击后重新跳转到最后一个版面
+                    $innerContent.animate({
+                        opacity: 0,
+                        left: -rollingWidth * (totalPage - 1)}, 0)
+                        .animate({opacity:1},"normal");
+                    cur = totalPage;
+                }
+                else{
+                    $innerContent.animate({
+                        left: "+=" + rollingWidth  //切换版面
+                    }, "normal");
+                    cur -= 1;
+                }
+                Rolling.curDot(cur);  //当前版面的圆点提示
+            }
+        });                            
+    }
 }
