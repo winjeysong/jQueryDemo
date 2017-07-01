802085bfc97fcaf55ff2715954c3339b168a8638
winjeysong <winjey-song@163.com>
2017-07-02 02:13:10 +0800

    Fix the bug & others
    1. Make “cur” be global variable
    2. Improve some details
    3. Test when there are 14 “li”s, it still works well

--- js/rolling_left_right.js
+++ js/rolling_left_right.js
@@ -1,10 +1,12 @@
 /**
- * 用于左右滚动的展示页面，其中提示圆点可以改成其他图片
+ * 用于左右滚动的展示页面，
  * winjeysong
  */
     
 //基本参数   
 var num = 4;  //每个版面的图片数
+var cur = 1;  //初始化当前页数
+/*cur作为全局变量的原因：方法dotRolling和rollingRightLeft共用该变量，每次触发click事件都会刷新cur值，这样就不会出现点完最后一个圆点后，再点向右箭头，会出现一段空白内容的情况*/
     
 var $wrap = $("span.next").parents("div.wrap");
 var $innerContent = $wrap.find("div.inner_content");
@@ -42,14 +44,13 @@ var Rolling = {
             if(! $innerContent.is(":animated")){
                 $innerContent.animate({left: - rollingWidth * dotIndex});  //控制innercontent的位置
                 Rolling.curDot(dotIndex + 1);  //点击后，当前版面的圆点提示
+                cur = dotIndex + 1;  //将cur重新赋值为当前所点击的圆点的索引值+1
             }
         });
     },
 
     //左右箭头功能：点击后展示下一版面或上一版面
     rollingRightLeft: function(){
-        let cur = 1;  //初始化当前页数     
-
         //右箭头      
         $("span.next").click(function(){
             if(! $innerContent.is(":animated")){  //如果元素当前不处于动画中，才为它添加动画。防止快速点击鼠标后，即使放开鼠标后，还是继续产生滚动动画的情况发生
